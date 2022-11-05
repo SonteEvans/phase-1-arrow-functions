5fcfea76003a56a423f9c19b0228423687e2dd22
Jahsonte <jahsonteevans@gmail.com>
2022-11-05 16:58:44 -0600

    commit tree fde04946e7e06a78835a62859bc52753ca4d570b

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = (d, b) => {
+    return d / b;
+}
+console.log(2000 / 100)
+
+const square = (z) => {
+    return z * z;
+}
+
+const add = (o, v) => {
+    return o + v
+}
+console.log(5 + 5)
